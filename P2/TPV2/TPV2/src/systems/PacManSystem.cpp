930e870eeb19f706ea105b70100a66e3e385461a
Mikeey84 <milope21@ucm.es>
2024-03-18 18:05:39 +0100

    se mueve

--- P2/TPV2/TPV2/src/systems/PacManSystem.cpp
+++ P2/TPV2/TPV2/src/systems/PacManSystem.cpp
@@ -36,42 +36,33 @@ void PacManSystem::update() {
 	if (ihldr.keyDownEvent()) {
 
 		if (ihldr.isKeyDown(SDL_SCANCODE_RIGHT)) { // rotate right
-			pmTR_->rot_ = pmTR_->rot_ + 5.0f;
+			pmTR_->rot_ = pmTR_->rot_ + 90.0f;
 
 			// also rotate the PacMan so it looks in the same
 			// direction where it moves
 			//
-			pmTR_->vel_ = pmTR_->vel_.rotate(5.0f);
+			pmTR_->vel_ = pmTR_->vel_.rotate(90.0f);
 		} else if (ihldr.isKeyDown(SDL_SCANCODE_LEFT)) { // rotate left
-			pmTR_->rot_ = pmTR_->rot_ - 5.0f;
+			pmTR_->rot_ = pmTR_->rot_ - 90.0f;
 
 			// also rotate the PacMan so it looks in the same
 			// direction where it moves
 			//
-			pmTR_->vel_ = pmTR_->vel_.rotate(-5.0f);
+			pmTR_->vel_ = pmTR_->vel_.rotate(-90.0f);
 		} else if (ihldr.isKeyDown(SDL_SCANCODE_UP)) { // increase speed
 
-			// add 1.0f to the speed (respecting the limit 3.0f). Recall
-			// that speed is the length of the velocity vector
-			float speed = std::min(3.0f, pmTR_->vel_.magnitude() + 1.0f);
-
-			// change the length of velocity vecto to 'speed'. We need
-			// '.rotate(rot)' for the case in which the current speed is
-			// 0, so we rotate it to the same direction where the PacMan
-			// is looking
-			//
-			pmTR_->vel_ = Vector2D(0, -speed).rotate(pmTR_->rot_);
+			pmTR_->vel_ = Vector2D(0.0f, -3.0f).rotate(pmTR_->rot_);
 		} else if (ihldr.isKeyDown(SDL_SCANCODE_DOWN)) { // decrease speed
 			// subtract 1.0f to the speed (respecting the limit 0.0f). Recall
 			// that speed is the length of the velocity vector
-			float speed = std::max(0.0f, pmTR_->vel_.magnitude() - 1.0f);
+			
 
 			// change the length of velocity vector to 'speed'. We need
 			// '.rotate(rot)' for the case in which the current speed is
 			// 0, so we rotate it to the same direction where the PacMan
 			// is looking
 			//
-			pmTR_->vel_ = Vector2D(0, -speed).rotate(pmTR_->rot_);
+			pmTR_->vel_ = Vector2D(0, 0).rotate(pmTR_->rot_);
 		}
 
 	}
